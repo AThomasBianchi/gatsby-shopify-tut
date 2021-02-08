d5e84f15208cbff3f8211e3563b597a6300f9376
Bianchi <bianchi@Bianchi-Pro.attlocal.net>
2021-02-08 07:19:36 -0500

    add preview image gallery

--- src/components/ImageGallery/index.js
+++ src/components/ImageGallery/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'gatsby-image';
 import { ImageGalleryWrapper } from './styles';
+import ImageThumbnail from './ImageThumbnail';
 
 export function ImageGallery({ images }) {
+  const [activeImageThumbnail, setActiveImageThumbail] = useState(images[0])
+  
+  const handleClick = (image) => {
+    setActiveImageThumbail(image)
+  }
+
   return (
     <ImageGalleryWrapper>
       <div>
-        <Image fluid={images[0].localFile.childImageSharp.fluid}/>
+        <Image fluid={activeImageThumbnail.localFile.childImageSharp.fluid}/>
+      </div>
+      <div>
+        {images.map(image => {
+          return <ImageThumbnail 
+            key={image.id} 
+            image={image}
+            onClick={handleClick}
+            isActive={image.id === activeImageThumbnail.id}/>
+        })}
       </div>
     </ImageGalleryWrapper>
   );
