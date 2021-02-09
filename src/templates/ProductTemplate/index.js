2f8b5b1d6d1b6efd1af986970b44575854193efe
Bianchi <bianchi@Bianchi-Pro.attlocal.net>
2021-02-09 07:27:13 -0500

    query shopify for stock

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { graphql } from 'gatsby';
 import { Layout, ImageGallery } from 'components';
-import { Grid } from './styles';
+import { Grid, SelectWrapper, Price } from './styles';
+import CartContext from 'context/CartContext'
 
 
 export const query = graphql`
   query ProductQuery($shopifyId: String){
     shopifyProduct(shopifyId: {eq: $shopifyId}) {
+      shopifyId
       title
       description
       images {
@@ -24,14 +26,46 @@ export const query = graphql`
 `;
 
 export default function ProductTemplate(props) {
+  const { getProductById } = useContext(CartContext);
+  const [product, setProduct] = useState(null) 
+  const [selectedVariant, setSelectedVariant] = useState(null)
   const { data } = props;
-  console.log(props);
+
+  useEffect(() => {
+    getProductById(data.shopifyProduct.shopifyId).then(result => {
+      setProduct(result)
+      setSelectedVariant(result?.variants[0]);
+    })
+  }, [getProductById, setProduct]);
+
+  const handleVariantChange = (e) => {
+    setSelectedVariant(product?.variants.find(v => v.id === e.target.value));
+  }
+
   return (
     <Layout>
       <Grid>
         <div>
           <h1>{data.shopifyProduct.title}</h1>
           <p>{data.shopifyProduct.description}</p>
+          { product?.availableForSale &&
+            <>
+            <SelectWrapper>
+              <strong>Variant</strong>
+              <select onChange={handleVariantChange}>
+              {product?.variants.map(v => (
+                <option 
+                  key={v.id}
+                  value={v.id}
+                >
+                  {v.title}
+                </option>
+              ))}
+              </select>
+            </SelectWrapper>
+            {!!selectedVariant && <Price>${selectedVariant.price}</Price>}
+            </>
+          }
         </div>
         <div>
           <ImageGallery images={data.shopifyProduct.images} />
